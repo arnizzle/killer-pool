3a6305d457246bbf5c6ca5852bb783c77e9319ea
arnizzle <arne.polman@gmail.com>
2026-01-28 22:01:34 +0100

    Blah

---
+++ commit.sh
@@ -0,0 +1,10 @@
+# Source - https://stackoverflow.com/q
+# Posted by mgold, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-01-28, License - CC BY-SA 3.0
+
+#!/bin/bash  
+git add .  
+read -p "Commit description: " desc  
+git commit -m "$desc"
+git push origin master
+
